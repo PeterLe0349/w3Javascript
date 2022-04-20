7fbea7c992442e83c1e37816e08f645da1fa3516
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-04-20 09:05:01 -0500

    added add function

--- js/zzzDailyPractice042022.js
+++ js/zzzDailyPractice042022.js
@@ -1,5 +1,36 @@
 "use strict";
-(function(){
+    let txt = '', myName ='';
     console.log("Daily log practice 04 20 22");
+    let str = 'asdljl  dfkljd';
+    let spacey = false;
+    spacey =str.includes('asdf');
+    console.log("Does it have space? " + spacey);
 
-})()
+
+    function greeting(){
+        myName = prompt("What is your name?");
+        myName = `Good morning ${myName}!`;
+        txt += myName;
+        document.getElementById('login').innerHTML = txt;
+    }
+
+    function doTask(){
+        let task = prompt("What do you want to do? (available features: add)");
+        switch(task){
+            case 'add':
+                add();
+                break;
+            default:
+                alert("You chose to do nothing...");
+                break;
+        }
+    }
+
+    function add(x,y){
+        x = parseInt(prompt("What is the first number?"));
+        y = parseInt(prompt("What is the second number"));
+        alert(`The sum is ${x+y}`);
+    }
+
+
+
