46d898f5919e84017dc2e660bc10c5fe45e4d131
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-04-23 15:42:57 -0500

    added string change with pop and push

--- js/zzzDailyPractice042322.js
+++ js/zzzDailyPractice042322.js
@@ -1,7 +1,7 @@
 "use strict";
 (function(){
     console.log("Hello Peter, log 04 23 22");
-    const nums = [3, 34, 234, 543, 23, 23];
+    const nums = [3, 34, 234, 543, 23, 23, -800];
     const colors = ['red', 'blue', 'white', 'green', 'purple'];
     let me = {firstName:'Peter', lastName:'Le',race:'asian',isVegan:false};
 
@@ -10,6 +10,24 @@
     document.getElementById("btn").addEventListener("click", testArea);
     document.getElementById('me').addEventListener('click', showMe);
     document.getElementById('loopData').addEventListener('click', loopie);
+    document.getElementById('stringChange').addEventListener('click', stringChangie);
+
+    function stringChangie(){
+        let txt = '';
+        txt += colors;
+        colors.push('cyan');
+        colors.unshift('aqua');
+        txt += '<br>'+colors;
+        colors.pop();
+        colors.pop();
+        colors.pop();
+        colors.pop();
+        txt += '<br>'+colors;
+
+
+
+        document.getElementById('stringChange').innerHTML = txt;
+    }
 
 function showMe(){
     document.getElementById('me').innerHTML= me.firstName + ' ' + me.lastName + '. Race: ' + me.race;
@@ -26,9 +44,9 @@ function testArea(){
 function loopie(){
 
     let result = 0;
-    for(let i=0; i<= 5; i++){
-        result += i;
-    }
+   for(let i in nums){
+       result += nums[i];
+   }
     document.getElementById('loopData').innerHTML = result.toString();
 
 }
