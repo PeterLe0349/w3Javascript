28ca758cff49744d00bea397e1aec5805173b206
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-04-24 15:42:09 -0500

    added switch and add

--- js/zzzDailyPractice042422.js
+++ js/zzzDailyPractice042422.js
@@ -5,7 +5,27 @@
     document.getElementById('btn').addEventListener('click', greet);
     function greet(){
         let myName = prompt("Enter username");
-        document.getElementById("greeting").innerHTML = `Username: ${myName}<br>What would you like to do today ,${myName}`;
+        document.getElementById("greeting").innerHTML = `Username: ${myName}<br>What would you like to do today, ${myName}`;
+    }
+    //task
+    document.getElementById('taskBtn').addEventListener('click', task);
+    function task(){
+        let task = '';
+        task = prompt("What do you want to do?");
+        switch(task){
+            case 'add':
+                task = add(100,4);
+                break;
+            default:
+                task = 'Nothing decided'; break;
+        }
+        document.getElementById("taskData").innerHTML = task;
+    }
+
+    function add(x,y){
+        x =  parseInt(prompt("First number?"));
+        y =  parseInt(prompt("Second number?"));
+        return `The sum of ${x} and ${y} is ${x+y}`;
     }
 
 })()
