9a5fac81b1f2225606243858488c1abd6316e9f9
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-04-26 18:21:58 -0500

    more html stuff

---
+++ js/message.js
@@ -0,0 +1,10 @@
+'use strict';
+const message = () => {
+    const name = "Jesse";
+    const age = 40;
+    return name + ' is ' + age + 'years old.';
+};
+
+export default message;
+
+import { name, age } from "./person.js";
---
+++ js/zzzDailyPractice042622.js
@@ -0,0 +1,5 @@
+"use strict";
+//save the DOM to an object and use it that way to reduce slowdown
+const mainStuff = document.getElementById('demo');
+
+mainStuff.innerHTML = "yeah";
