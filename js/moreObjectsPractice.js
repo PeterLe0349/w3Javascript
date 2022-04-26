005916068d0f7507516a33f3b45aee2de32dea88
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-04-26 08:51:43 -0500

    more obj practice

--- js/moreObjectsPractice.js
+++ js/moreObjectsPractice.js
@@ -1,6 +1,22 @@
 "use strict";
-(function(){
+//(function(){
     console.log("More Objects Practice hello");
+
+    const dog = {};
+    dog.age = 9;
+    dog.breed = 'Doberman';
+    dog.bark = function(){
+        console.log('Woof');
+    };
+
+    
+
+
+
+
+
+
+
 
     function makeCar(make, model){
         return {make, model};
@@ -19,13 +35,31 @@
     salesReport.sayOffice = function(){
         console.log("The office name is: " + this.office);
     }
-    salesReport.sayOffice();
+    //salesReport.sayOffice();
 
     function createEmployee(employeeNumber, firstName, lastName, salesUnit){
         return {employeeNumber, firstName, lastName, salesUnit};
     }
-    salesReport.employees.push(createEmployee(13, 'Bob', 'Dole', 5));
+    salesReport.employees.push(createEmployee(13, 'Bob', 'Dole', 6));
+    let emp2 = createEmployee(15, 'Mike', 'Twain', 27);
+    salesReport.employees.push(emp2);
+    //console.log(salesReport.employees);
+    salesReport.getEmployeeCount = function(){
+        console.log("Total employee count is: " + salesReport.employees.length);
+    }
+
+    salesReport.getTotalNumberOfSales = function(){
+        let totalSales = 0;
+        for(let i = 0; i < salesReport.employees.length;i++){
+            totalSales += salesReport.employees[i].salesUnit;
+        }
+        return totalSales;
+    }
+
+    salesReport.getAverageSalesPerEmployee = function() {
+        return this.getTotalNumberOfSales() / salesReport.employees.length ;
+    }
 
     //console.log(salesReport);
 
-})();
+//})();
