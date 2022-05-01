aa6e6df9eecb341b9b9a8d593f22f0390d6db527
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-01 13:02:14 -0500

    add event and listener

--- js/zzzDailyJS050122.js
+++ js/zzzDailyJS050122.js
@@ -2,4 +2,11 @@
 (function(){
     console.log("hello from 05 01 22");
 
+    document.getElementById("apostleButton").addEventListener("click", apostle);
+
+    function apostle(){
+        let whoApostle = prompt("What do you want to ask?");
+        document.getElementById("apostleText").innerHTML = `The answer to your ${whoApostle} is: Believe. Do.`;
+    }
+
 })();
