00567be03a00acc48464fbdaaebe115bb93c9da5
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-14 16:43:31 -0500

    setinterval and clearinterval

--- js/zzzDailyJS051422.js
+++ js/zzzDailyJS051422.js
@@ -2,5 +2,34 @@
 (function(){
     console.log("hello 05 14");
 
+    let timerCount = 10;
+    let bomber = {};
+
+    function startFuse(){
+        bomber = setInterval(function() {
+            console.log(`You have ${timerCount} seconds to defuse the bomb!`);
+            timerCount--;
+            if (timerCount === 0) {
+                alert("Baboom!");
+                clearInterval(bomber);
+            }
+        }, 1000);
+    }
+
+    document.getElementById('fuse').addEventListener('click', startFuse);
+    document.getElementById("defuse").addEventListener('click', defuse);
+
+
+
+    function defuse(){
+        console.log("Red wire cut!  BOMB defused! ");
+        clearInterval(bomber);
+    }
+
+
+
+
+
+
 
 })();
