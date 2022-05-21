d3699d33d2193b2169d4cbd2501acb9d6078a0e4
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-21 15:40:15 -0500

    daily rev

--- js/zzzDailyJS052122.js
+++ js/zzzDailyJS052122.js
@@ -1,2 +1,18 @@
 "use strict";
 console.log("hello 21");
+let click = -1;
+const myCarouselElement = document.querySelector('#carouselExampleIndicators');
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 300,
+    wrap: true
+});
+document.querySelector('#logSubBtn').addEventListener('click', function(e) {
+    e.preventDefault();
+    click *= -1;
+    console.log(click);
+    if(click === -1){
+        carousel.pause = true;
+    }else if(click === 1){
+        carousel.cycle();
+    }
+    });
