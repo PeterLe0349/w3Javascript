7e250110ddd8c593940174e8800b8a15a1e35511
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-21 10:34:55 -0500

    some stuff

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const OPEN_WEATHER_APPID = '06376542eafa236b267b115f9c22ef87';
-const MAPBOX_KEY = 'pk.eyJ1IjoiZmllcmVtIiwiYSI6ImNsM2RneDdkeTA1cWEza3Frd204MDA2ajYifQ.kuXTiyC1aNTX8dEyMKUMjQ';
-    
