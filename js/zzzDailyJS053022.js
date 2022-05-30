99d18fff60f708c080dacdfe9777793343471458
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-30 09:28:31 -0500

    review

--- js/zzzDailyJS053022.js
+++ js/zzzDailyJS053022.js
@@ -1,7 +1,37 @@
 "use strict";
 $(document).ready(function(){
-   $('#displayMain').html("<h1>Hello Main Jquery</h1>");
-   document.getElementById('displayLeft').innerHTML = '<strong>Hello Leftie</strong>';
-   document.getElementById('displayRight').innerHTML = '<i>Hello Rightie</i>';
+   $('#displayMain').html("<h1>Hello Main Jquery</h1>" + getProd());
+   document.getElementById('displayLeft').innerHTML = '<strong>Hello Leftie</strong>' + getSum(9);
+   document.getElementById('displayRight').innerHTML = '<i>Hello Rightie</i>' + getDif(9);
+
+   function getSum(amount){
+       let html = '<br>';
+       let sum = 0;
+       for(let i = 0; i< 10; i++){
+           html += `${i} + ${sum} = ${i+sum} <br>`;
+           sum += i;
+       }
+       return html;
+   }
+
+    function getDif(amount){
+        let html = '<br>';
+        let sum = 100;
+        for(let i = 0; i< 10; i++){
+            html += `${sum} - ${i}  = ${sum-i} <br>`;
+            sum -= i;
+        }
+        return html;
+    }
+
+    function getProd(amount){
+        let html = '<br>';
+        let sum = 1;
+        for(let i = 1; i< 10; i++){
+            html += `${sum} * ${i}  = ${sum*i} <br>`;
+            sum *= i;
+        }
+        return html;
+    }
 
 });
