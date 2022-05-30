c8bfc7c56b49343c1579d8dcd491539daf8794ed
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-30 09:23:39 -0500

    daily rev

---
+++ js/zzzDailyJS053022.js
@@ -0,0 +1,7 @@
+"use strict";
+$(document).ready(function(){
+   $('#displayMain').html("<h1>Hello Main Jquery</h1>");
+   document.getElementById('displayLeft').innerHTML = '<strong>Hello Leftie</strong>';
+   document.getElementById('displayRight').innerHTML = '<i>Hello Rightie</i>';
+
+});
