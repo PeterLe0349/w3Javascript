022e134d982b29ae4b38bea5840da2249aa12849
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-31 10:45:31 -0500

    grandma

---
+++ js/grandma.js
@@ -0,0 +1,21 @@
+"use strict";
+$(document).ready(function(){
+    let message = 'Take care Grandma.  May your life in heaven be peaceful and happy.';
+    $('#displayLeft').html(`<h1> Love you Grandma!  </h1>`);
+    document.getElementById('displayMain').innerHTML = message;
+    $('#displayRight').html('Love, Peter');
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // end jquery
+});
