3bd21bc3531e6c7de3647aa26e5a97baf50e3fdf
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-05-31 20:23:37 -0500

    review

---
+++ js/zzzDailyJS053122.js
@@ -0,0 +1,8 @@
+"use strict";
+$(document).ready(function(){
+    let mainMessage = `<h1 class="text-center">Hello 31 from Jquery</h1>`;
+    document.getElementById('displayMain').innerHTML = mainMessage;
+
+
+    // end jquery
+});
