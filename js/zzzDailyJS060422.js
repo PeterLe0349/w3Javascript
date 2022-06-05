69995cf3c7ce9c812aa4014525842e026a5e9aa4
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-06-04 20:18:25 -0500

    up

--- js/zzzDailyJS060422.js
+++ js/zzzDailyJS060422.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     // alert('hi 4');
     $('#displayMain').append(`<p>hello jun 4</p>`).append(`<p>hello jun 4.2</p>`).append(`<p>hello jun 4.3</p>`);
     document.getElementById('displayMain').setAttribute('class','text-center');
+    const myPromise = fetch(`https://sequoia-fuchsia-woolen.glitch.me/movies`);
+    // myPromise.then( promise => promise.json().then( resolve => console.log(resolve)));
 
 
 
