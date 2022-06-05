c9a3b0acd4984c7a710e791a9527c6efa4e2703a
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-06-04 22:05:42 -0500

    up

--- js/zzzDailyJS060422.js
+++ js/zzzDailyJS060422.js
@@ -1,6 +1,7 @@
 "use strict";
 $(document).ready(function(){
     // alert('hi 4');
+    let mainDisplay = document.getElementById('mainDisplay');
     $('#displayMain').append(`<p>hello jun 4</p>`).append(`<p>hello jun 4.2</p>`).append(`<p>hello jun 4.3</p>`);
     document.getElementById('displayMain').setAttribute('class','text-center');
     const myPromise = fetch(`https://sequoia-fuchsia-woolen.glitch.me/movies`);
@@ -19,6 +20,24 @@ $(document).ready(function(){
     }
 
     generateMainDisplayInformation(10);
+    let paraz = document.createElement("li");
+    paraz.append("hello");
+    console.log(paraz);
+    // document.getElementById('mainDisplay').appendChild(para);
+
+    // Create a p element:
+    const para = document.createElement("p");
+
+// Create a text node:
+    const node = document.createTextNode("This is a paragraph.");
+
+// Append text node to the p element:
+    para.appendChild(node);
+    console.log(para);
+
+// Append the p element to the body:
+    document.getElementById('displayMain').append(paraz);
+
 
 
 
