c41fbe0aac4f1fd553e8166d7bad1dbb676b26dd
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-06-29 09:06:15 -0500

    daily review

---
+++ js/zzzDailyJS062922.js
@@ -0,0 +1,18 @@
+"use strict";
+$(document).ready(function(){
+    console.log("hello 29");
+    fetch(GLITCH_MOVIE_URL).then(res => res.json()).then(movies => {
+        movies.forEach(movie=> {
+            document.getElementById("displayMain").innerHTML += `
+            <div>
+            <h4>Title: ${movie.title}</h4>
+            <p>Plot: ${movie.plot}</p>
+</div>`;
+
+
+        })
+    })
+
+
+    // end jquery
+});
