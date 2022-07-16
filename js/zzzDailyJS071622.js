70dfdc81672d815558a2aa7f29acd3b22e14d973
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-07-16 10:51:09 -0500

    daily rev

---
+++ js/zzzDailyJS071622.js
@@ -0,0 +1,16 @@
+"use strict";
+$(document).ready(function (){
+    console.log("hello 16th");
+    fetch((GLITCH_MOVIE_URL)).then(res => res.json()).then(movies => {
+        movies.forEach(movie => {
+            document.getElementById("displayMain").innerHTML += `
+            <div>
+            <h3>Title: ${movie.title}</h3>
+            <p>Plot: ${movie.plot}</p>
+</div>`;
+        })
+    })
+
+
+    // end jquery
+})    ;
