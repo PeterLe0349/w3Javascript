fee90e290ae3c79cf4baea999c1901e65a89598e
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-09-04 17:53:39 -0500

    update

---
+++ js/zzzDailyJS090422.js
@@ -0,0 +1,10 @@
+"use strict";
+$(document).ready(function(){
+    for(let x = 0; x < 10; x++){
+        document.getElementById("displayLeft").innerHTML += `The number is ${x*x}<br>`;
+    }
+
+
+
+    // end jquery
+})    ;
